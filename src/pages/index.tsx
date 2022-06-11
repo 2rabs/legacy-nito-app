9e097807034cb9e5bdc3ab7aed6e7c9ec0c91e5f
Ryo Takeuchi <develop@r09i.me>
2022-06-11 21:13:27 +0900

    build: UI ライブラリを追加
    UI ライブラリを追加しました。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
       </Head>
 
       <main className={styles.main}>
-        <h1>create-liff-app</h1>
+        <h1 className="text-3xl font-bold underline">create-liff-app</h1>
         {liff && <p>LIFF init succeeded.</p>}
         {liffError && (
           <>
