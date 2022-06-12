669a5c1dd6a7036a7f78a34673931983bbf350b1
Ryo Takeuchi <develop@r09i.me>
2022-06-12 21:55:32 +0900

    feat: liff の hook を追加
    liff の hook を追加しました。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import type { Liff } from "@line/liff";
-import { useState, useEffect } from "react";
+import { useLiff } from "@/hooks/core/liff";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const [liffObject, setLiffObject] = useState<Liff | null>(null);
-  const [liffError, setLiffError] = useState<string | null>(null);
-
-  // Execute liff.init() when the app is initialized
-  useEffect(() => {
-    // to avoid `window is not defined` error
-    import("@line/liff")
-      .then((liff) => liff.default)
-      .then((liff) => {
-        console.log("LIFF init...");
-        liff
-          .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
-          .then(() => {
-            console.log("LIFF init succeeded.");
-            setLiffObject(liff);
-          })
-          .catch((error: Error) => {
-            console.log("LIFF init failed.");
-            setLiffError(error.toString());
-          });
-      });
-  }, []);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { liffObject, liffError } = useLiff(process.env.NEXT_PUBLIC_LIFF_ID!);
 
   // Provide `liff` object and `liffError` object
   // to page component as property
   pageProps.liff = liffObject;
   pageProps.liffError = liffError;
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
