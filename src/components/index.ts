0af6ad86a82f63b085f56b0713f29ee542f331ce
Ryo Takeuchi <develop@r09i.me>
2022-06-25 08:45:25 +0900

    :sparkles: ロゴを追加
    ロゴを追加しました。

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,6 @@ export * from './BottomNavigation';
 export * from './Head';
 export * from './LayoutWrapper';
 export * from './LiffProvider';
+export * from './Logo';
 export * from './SectionContainer';
+export * from './SquareLogo';
