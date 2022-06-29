7959e3ac75149ff0609fa29c442c38908d09e90a
Ryo Takeuchi <develop@r09i.me>
2022-06-29 22:01:04 +0900

    :sparkles: noindex を追加
    検索インデックス登録をブロックするため noindex を追加しました。
    
    - https://developers.google.com/search/docs/advanced/crawling/block-indexing?hl=ja

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -11,6 +11,7 @@ const Head = (props: Props) => {
     <NextHead>
       <title>{ title }</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+      <meta name="robots" content="noindex" />
       <link rel="icon" href="/favicon.ico"/>
     </NextHead>
   );
