6bbd83243c715218ab8293c0e3aa287ff1970dc3
Ryo Takeuchi <develop@r09i.me>
2022-07-15 22:20:06 +0900

    :recycle: メインレイアウトコンポーネントを定義
    各ページ共通で利用するメインレイアウトコンポーネントを作成しました。

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -2,28 +2,28 @@ import { supabaseClient } from '@supabase/auth-helpers-nextjs';
 import { useUser as useSupabaseUser } from '@supabase/auth-helpers-react';
 import { Auth } from '@supabase/ui';
 import { NextPage } from 'next';
-import { Head } from '@/components';
+import { MainLayout } from "@/components/Layout";
 import { useCurrentUser } from '@/hooks/useCurrentUser';
 import styles from '@/styles/Home.module.css';
 
 const AuthScreen: NextPage = () => {
-  const { isAuthChecking, currentUser } = useCurrentUser();
-  const { user: authUser, error } = useSupabaseUser();
+  const {isAuthChecking, currentUser} = useCurrentUser();
+  const {user: authUser, error} = useSupabaseUser();
 
   return (
-    <div>
-      <Head pageName={'Auth'} />
-
-      <main className={styles.main}>
+    <MainLayout
+      title='Auth'
+    >
+      <main className={ styles.main }>
         <Auth
-          supabaseClient={supabaseClient}
+          supabaseClient={ supabaseClient }
           // providers={ ['google'] }
           socialLayout='horizontal'
           socialButtonSize='xlarge'
           // view='magic_link'
         />
       </main>
-    </div>
+    </MainLayout>
   );
 };
 
