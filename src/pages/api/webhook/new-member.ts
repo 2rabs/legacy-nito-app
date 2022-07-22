a73fb7378fad0de110e856c6619fc53ad81b9480
Ryo Takeuchi <develop@r09i.me>
2022-07-22 21:08:30 +0900

    :sparkles: メンバー加入のウェブフックを追加
    メンバー加入のウェブフックを追加しました。

---
+++ src/pages/api/webhook/new-member.ts
@@ -0,0 +1,31 @@
+import { Client as LineBotClient } from '@line/bot-sdk';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const config = {
+  channelAccessToken: process.env.LINE_MESSAGING_CHANNEL_TOKEN!,
+  channelSecret: process.env.LINE_MESSAGING_CHANNEL_SECRET!,
+};
+
+type Message = {
+  message: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Message>) {
+  if (req.body.secret !== process.env.WEBHOOK_SUPABASE_TABLE_SECRET) {
+    return res.status(401).json({ message: 'Your secret is invalid !' });
+  }
+
+  const client = new LineBotClient(config);
+
+  try {
+    await client.pushMessage(process.env.LINE_ADMIN_GROUP_ID!, {
+      type: 'text',
+      text: `${req.body.record.nickname} さんがメンバーに加わりました 🎉`,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  res.json({
+    message: 'OK',
+  });
+}
